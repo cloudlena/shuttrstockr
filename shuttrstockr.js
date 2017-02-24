be9cee958a3701ddf5c32c321a9a9748f6853900
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-02-24 15:15:09 +0100

    Change indentation to 4 spaces

--- shuttrstockr.js
+++ shuttrstockr.js
@@ -2,14 +2,14 @@
 
 /* eslint-disable max-len */
 
-'use strict';
+"use strict";
 
-const appInfo = require('./package');
-const fs = require('fs');
-const os = require('os');
-const program = require('commander');
-const shutterstock = require('shutterstock');
-const request = require('request');
+const appInfo = require("./package");
+const fs = require("fs");
+const os = require("os");
+const program = require("commander");
+const shutterstock = require("shutterstock");
+const request = require("request");
 
 /**
  * Download a file
@@ -19,120 +19,120 @@ const request = require('request');
  * @returns {void}
  */
 function download(uri, filename, callback) {
-  request.head(uri, (err) => {
-    if (err) throw err;
+    request.head(uri, (err) => {
+        if (err) throw err;
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
+        request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
+    });
 }
 
 program.version(appInfo.version);
 
 program
-  .command('search <searchTerm>')
-  .alias('s')
-  .option('-i, --id <Client ID>', 'Your Shutterstock API Client ID')
-  .option('-s, --secret <Client Secret>', 'Your Shutterstock API Client Secret')
-  .option('-p, --path [path]', 'The location to save the images to')
-  .option('-n, --number <number>', 'The number of images to download')
+  .command("search <searchTerm>")
+  .alias("s")
+  .option("-i, --id <Client ID>", "Your Shutterstock API Client ID")
+  .option("-s, --secret <Client Secret>", "Your Shutterstock API Client Secret")
+  .option("-p, --path [path]", "The location to save the images to")
+  .option("-n, --number <number>", "The number of images to download")
   .option(
-    '-a, --abbreviation <abbreviation>',
-    'The prefix to add to the image name'
+    "-a, --abbreviation <abbreviation>",
+    "The prefix to add to the image name"
   )
-  .description('Search the Shutterstock API')
+  .description("Search the Shutterstock API")
   .action((searchTerm, options) => {
-    const query = {};
-    const api = shutterstock.v2({
-      clientId: options.id,
-      clientSecret: options.secret
-    });
-    let numPages = 1;
-    let keywords = '';
-    let resultString = '';
+      const query = {};
+      const api = shutterstock.v2({
+          clientId: options.id,
+          clientSecret: options.secret,
+      });
+      let numPages = 1;
+      let keywords = "";
+      let resultString = "";
 
-    query.query = searchTerm;
+      query.query = searchTerm;
 
-    query.license = [
-      'commercial',
-      'enhanced'
-    ];
+      query.license = [
+          "commercial",
+          "enhanced",
+      ];
 
-    if (options.path) {
-      options.path = `${options.path}/`;
-    } else {
-      options.path = '';
-    }
-
-    if (options.number) {
-      if (options.number <= 50) {
-        query.per_page = options.number;
+      if (options.path) {
+          options.path = `${options.path}/`;
       } else {
-        query.per_page = 50;
-        numPages = Math.floor(options.number / 50);
+          options.path = "";
       }
-    }
 
-    for (let j = 0; j < numPages; j++) {
-      query.page = j + 1;
+      if (options.number) {
+          if (options.number <= 50) {
+              query.per_page = options.number;
+          } else {
+              query.per_page = 50;
+              numPages = Math.floor(options.number / 50);
+          }
+      }
 
-      let i = 0;
+      for (let j = 0; j < numPages; j++) {
+          query.page = j + 1;
+
+          let i = 0;
 
       api.image.search(query, (err1, data1) => { // eslint-disable-line
-        if (err1) throw err1;
+          if (err1) throw err1;
 
         /**
-         * get a single image and download it
+         * Get a single image and download it
          * @returns {void}
          */
-        function getImageDetails() {
-          const fromPrevPages = 50 * j;
-          const imgIndex = i + fromPrevPages;
+          function getImageDetails() {
+              const fromPrevPages = 50 * j;
+              const imgIndex = i + fromPrevPages;
 
-          keywords = '';
-          setTimeout(() => {
+              keywords = "";
+              setTimeout(() => {
 
-            api.image.get(data1.data[i].id, (err2, data) => {
-              if (err2) throw err2;
+                  api.image.get(data1.data[i].id, (err2, data) => {
+                      if (err2) throw err2;
 
-              if (data.keywords) {
-                for (let k = 0; k < data.keywords.length; k++) {
-                  if (k === data.keywords.length - 1) {
-                    keywords += data.keywords[k];
-                  }
-                  keywords += `${data.keywords[k]}, `;
-                }
-              } else {
-                keywords = '';
-              }
-              resultString = `${imgIndex}|shutterstock-${options.abbreviation}-${imgIndex}-${data.id}|${keywords}|${data.id}${os.EOL}`;
-              fs.appendFile('data.txt', resultString, (err3) => {
-                if (err3) throw err3;
-              });
+                      if (data.keywords) {
+                          for (let k = 0; k < data.keywords.length; k++) {
+                              if (k === data.keywords.length - 1) {
+                                  keywords += data.keywords[k];
+                              }
+                              keywords += `${data.keywords[k]}, `;
+                          }
+                      } else {
+                          keywords = "";
+                      }
+                      resultString = `${imgIndex}|shutterstock-${options.abbreviation}-${imgIndex}-${data.id}|${keywords}|${data.id}${os.EOL}`;
+                      fs.appendFile("data.txt", resultString, (err3) => {
+                          if (err3) throw err3;
+                      });
 
-              download(
+                      download(
                 data.assets.preview.url,
                 `${options.path}shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`,
                 () => {
-                  console.log(`downloaded shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`);
+                    console.log(`downloaded shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`);
                 }
               );
-            });
+                  });
 
-            i++;
-            if (i < data1.data.length) {
-              getImageDetails();
-            }
+                  i++;
+                  if (i < data1.data.length) {
+                      getImageDetails();
+                  }
 
-          }, 1000);
-        }
-        getImageDetails();
+              }, 1000);
+          }
+          getImageDetails();
 
       });
-    }
+      }
   });
 
 program.parse(process.argv);
 
 if (!program.args.length) {
-  program.help();
+    program.help();
 }

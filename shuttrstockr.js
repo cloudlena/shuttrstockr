dbdbd3182cd621db467cb3e5b4d90abc5c40b7d0
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2019-09-03 13:05:37 +0200

    Update dependencies

--- shuttrstockr.js
+++ shuttrstockr.js
@@ -22,114 +22,111 @@ function download(uri, filename, callback) {
     request.head(uri, (err) => {
         if (err) throw err;
 
-        request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
+        request(uri)
+            .pipe(fs.createWriteStream(filename))
+            .on("close", callback);
     });
 }
 
 program.version(appInfo.version);
 
 program
-  .command("search <searchTerm>")
-  .alias("s")
-  .option("-i, --id <Client ID>", "Your Shutterstock API Client ID")
-  .option("-s, --secret <Client Secret>", "Your Shutterstock API Client Secret")
-  .option("-p, --path [path]", "The location to save the images to")
-  .option("-n, --number <number>", "The number of images to download")
-  .option(
-    "-a, --abbreviation <abbreviation>",
-    "The prefix to add to the image name"
-  )
-  .description("Search the Shutterstock API")
-  .action((searchTerm, options) => {
-      const query = {};
-      const api = shutterstock.v2({
-          clientId: options.id,
-          clientSecret: options.secret,
-      });
-      let numPages = 1;
-      let keywords = "";
-      let resultString = "";
+    .command("search <searchTerm>")
+    .alias("s")
+    .option("-i, --id <Client ID>", "Your Shutterstock API Client ID")
+    .option("-s, --secret <Client Secret>", "Your Shutterstock API Client Secret")
+    .option("-p, --path [path]", "The location to save the images to")
+    .option("-n, --number <number>", "The number of images to download")
+    .option(
+        "-a, --abbreviation <abbreviation>",
+        "The prefix to add to the image name"
+    )
+    .description("Search the Shutterstock API")
+    .action((searchTerm, options) => {
+        const query = {};
+        const api = shutterstock.v2({
+            clientId: options.id,
+            clientSecret: options.secret,
+        });
+        let numPages = 1;
+        let keywords = "";
+        let resultString = "";
 
-      query.query = searchTerm;
+        query.query = searchTerm;
 
-      query.license = [
-          "commercial",
-          "enhanced",
-      ];
+        query.license = ["commercial", "enhanced"];
 
-      if (options.path) {
-          options.path = `${options.path}/`;
-      } else {
-          options.path = "";
-      }
+        if (options.path) {
+            options.path = `${options.path}/`;
+        } else {
+            options.path = "";
+        }
 
-      if (options.number) {
-          if (options.number <= 50) {
-              query.per_page = options.number;
-          } else {
-              query.per_page = 50;
-              numPages = Math.floor(options.number / 50);
-          }
-      }
+        if (options.number) {
+            if (options.number <= 50) {
+                query.per_page = options.number;
+            } else {
+                query.per_page = 50;
+                numPages = Math.floor(options.number / 50);
+            }
+        }
 
-      for (let j = 0; j < numPages; j++) {
-          query.page = j + 1;
+        for (let j = 0; j < numPages; j++) {
+            query.page = j + 1;
 
-          let i = 0;
+            let i = 0;
 
-      api.image.search(query, (err1, data1) => { // eslint-disable-line
-          if (err1) throw err1;
+            api.image.search(query, (err1, data1) => {
+        // eslint-disable-line
+                if (err1) throw err1;
 
-        /**
-         * Get a single image and download it
-         * @returns {void}
-         */
-          function getImageDetails() {
-              const fromPrevPages = 50 * j;
-              const imgIndex = i + fromPrevPages;
+                /**
+                 * Get a single image and download it
+                 * @returns {void}
+                 */
+                function getImageDetails() {
+                    const fromPrevPages = 50 * j;
+                    const imgIndex = i + fromPrevPages;
 
-              keywords = "";
-              setTimeout(() => {
+                    keywords = "";
+                    setTimeout(() => {
+                        api.image.get(data1.data[i].id, (err2, data) => {
+                            if (err2) throw err2;
 
-                  api.image.get(data1.data[i].id, (err2, data) => {
-                      if (err2) throw err2;
+                            if (data.keywords) {
+                                for (let k = 0; k < data.keywords.length; k++) {
+                                    if (k === data.keywords.length - 1) {
+                                        keywords += data.keywords[k];
+                                    }
+                                    keywords += `${data.keywords[k]}, `;
+                                }
+                            } else {
+                                keywords = "";
+                            }
+                            resultString = `${imgIndex}|shutterstock-${options.abbreviation}-${imgIndex}-${data.id}|${keywords}|${data.id}${os.EOL}`;
+                            fs.appendFile("data.txt", resultString, (err3) => {
+                                if (err3) throw err3;
+                            });
 
-                      if (data.keywords) {
-                          for (let k = 0; k < data.keywords.length; k++) {
-                              if (k === data.keywords.length - 1) {
-                                  keywords += data.keywords[k];
-                              }
-                              keywords += `${data.keywords[k]}, `;
-                          }
-                      } else {
-                          keywords = "";
-                      }
-                      resultString = `${imgIndex}|shutterstock-${options.abbreviation}-${imgIndex}-${data.id}|${keywords}|${data.id}${os.EOL}`;
-                      fs.appendFile("data.txt", resultString, (err3) => {
-                          if (err3) throw err3;
-                      });
+                            download(
+                                data.assets.preview.url,
+                                `${options.path}shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`,
+                                () => {
+                                    console.log(`downloaded shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`);
+                                }
+                            );
+                        });
 
-                      download(
-                data.assets.preview.url,
-                `${options.path}shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`,
-                () => {
-                    console.log(`downloaded shutterstock-${options.abbreviation}-${imgIndex}-${data.id}.jpg`);
+                        i++;
+                        if (i < data1.data.length) {
+                            getImageDetails();
+                        }
+                    }, 1000);
                 }
-              );
-                  });
-
-                  i++;
-                  if (i < data1.data.length) {
-                      getImageDetails();
-                  }
-
-              }, 1000);
-          }
-          getImageDetails();
-
-      });
-      }
-  });
+                getImageDetails();
+            });
+        }
+    });
 
 program.parse(process.argv);
 
